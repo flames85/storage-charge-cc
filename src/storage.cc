169e655253a5cf5f002f48452f8b1db97a70cdc3
212715760 <212715760@ge.com>
2021-01-15 07:39:27 +0800

    use StorageType::charge instead of Storage::charge

--- src/storage.cc
+++ src/storage.cc
@@ -11,6 +11,42 @@ struct StorageType
 {
     virtual Storage::Type getType() const = 0;
     virtual ~StorageType() {}
+
+    virtual double charge(int capacity, int months) const
+    {
+        double price = 0;
+        switch (getType())
+        {
+        case Storage::Type::ST_BLOCK_STORAGE:
+            price += 40;
+            if (capacity > BASIC_BLOCK_SIZE)
+            {
+                double exceed = (double)(capacity)-BASIC_BLOCK_SIZE;
+                price += months * exceed * 3;
+            }
+            break;
+        case Storage::Type::ST_FILE_STORAGE:
+            price += 20;
+            if (months > BASIC_FILE_MONTHS)
+            {
+                double exceed = (double)(months)-BASIC_FILE_MONTHS;
+                price += exceed * 1.5;
+            }
+            break;
+        case Storage::Type::ST_OBJECT_STORAGE:
+            price += 10;
+            if (months > BASIC_OBJECT_MONTHS)
+            {
+                double exceed = (double)(months)-BASIC_OBJECT_MONTHS;
+                price += exceed * capacity * 1.5;
+            }
+            break;
+        default:
+            break;
+        }
+
+        return price;
+    }
 };
 
 struct BlockStorageType : public StorageType
@@ -74,38 +110,7 @@ Storage::Type Storage::getType() const
 
 double Storage::charge() const
 {
-    double price = 0;
-    switch (type->getType())
-    {
-    case Type::ST_BLOCK_STORAGE:
-        price += 40;
-        if (capacity > BASIC_BLOCK_SIZE)
-        {
-            double exceed = (double)(capacity) - BASIC_BLOCK_SIZE;
-            price += months * exceed * 3;
-        }
-        break;
-    case Type::ST_FILE_STORAGE:
-        price += 20;
-        if (months > BASIC_FILE_MONTHS)
-        {
-            double exceed = (double)(months) - BASIC_FILE_MONTHS;
-            price += exceed * 1.5;
-        }
-        break;
-    case Type::ST_OBJECT_STORAGE:
-        price += 10;
-        if (months > BASIC_OBJECT_MONTHS)
-        {
-            double exceed = (double)(months) - BASIC_OBJECT_MONTHS;
-            price += exceed * capacity * 1.5;
-        }
-        break;
-    default:
-        break;
-    }
-
-    return price;
+    return type->charge(capacity, months);
 }
 
 int Storage::levels() const
